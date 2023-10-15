822669ca330aa205fb1c1de898c18a2512b55cf3
Maycol <maycoldanielp@gmail.com>
2023-10-15 13:46:46 -0400

    feat: added loading state when fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { Suspense, useEffect } from 'react'
 import { useTaskStore } from './store'
 import { useModal } from './hooks'
 import { getMonthName, domiData } from './utils'
@@ -69,11 +69,19 @@ function App() {
             </div>
             <div className='bg-slate-50 h-[calc(100vh-144px)] rounded-tl-[30px] p-6 shadow-xl'>
                <ul className="flex flex-col gap-y-2">
-                  {tasks
-                     .sort((a, b) => Number(a.isFinished) - Number(b.isFinished)) // Sort by isFinished in ascending order
-                     .map((item, index) => (
-                        <TaskLi key={index} {...item} />
-                     ))}
+                  <Suspense fallback={
+                     <svg className="animate-spin mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                        <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                     </svg>
+                  }>
+                     {tasks
+                        .sort((a, b) => Number(a.isFinished) - Number(b.isFinished)) // Sort by isFinished in ascending order
+                        .map((item, index) => (
+                           <TaskLi key={index} {...item} />
+                        ))}
+                  </Suspense>
+                 
                </ul>
             </div>
          </section>
